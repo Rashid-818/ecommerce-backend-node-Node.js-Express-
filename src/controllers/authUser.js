722c763ffa5750f98dcc3f3016ac772a39e6b6e4
Rashid <rkbooy623@gmail.com>
2026-08-30 01:16:45 +0530

    Add login functionality and enhance signup response with JWT token

--- src/controllers/authUser.js
+++ src/controllers/authUser.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
-import { generateJWT, hashPassword } from "../utils/security.js";
+import { generateJWT, hashPassword, verifyPassword } from "../utils/security.js";
 
+
+// Signup Controllers
 const signUp = async (req, res) => {
     try {
         const { username, email, password } = req.body
@@ -20,9 +22,9 @@ const signUp = async (req, res) => {
                 }
             )
         }
-
+        
         const hashPass = await hashPassword(password)
-
+        
         const user = new User(
             {
                 username,
@@ -30,9 +32,9 @@ const signUp = async (req, res) => {
                 password: hashPass
             }
         )
-
+        
         await user.save()
-
+        
         const jwtToken = generateJWT(
             {
                 userId: user._id,
@@ -43,12 +45,13 @@ const signUp = async (req, res) => {
         return res.status(201).json(
             {
                 message: "User created 😊",
-                userId: user._id
+                userId: user._id,
+                jwtToken
             }
         )
     } catch (error) {
-        console.error("Signup Error Context: ",error);
-        
+        console.error("Signup Error Context: ", error);
+
         return res.status(500).json(
             {
                 message: "Server Error !"
@@ -57,6 +60,69 @@ const signUp = async (req, res) => {
     }
 }
 
+
+// Login Controllers
+const login = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        if ((!username && !email) || !password) {
+            return res.status(400).json(
+                {
+                    message: "All fields are required !"
+                }
+            )
+        }
+
+        const user = await User.findOne(
+            {
+                $or: [
+                    { email },
+                    {username}
+                ]
+            }
+        )
+        if (!user) {
+            return res.status(404).json({
+                message: "Invalid credentials"
+            })
+        }
+
+        const isMatch = await verifyPassword(password, user.password)
+        if (!isMatch) {
+            return res.status(401).json(
+                {
+                    message: "Invalid credentials"
+                }
+            )
+        }
+
+        const token = generateJWT(
+            {
+                userId: user._id,
+                email: user.email
+            }
+        )
+
+        return res.status(200).json(
+            {
+                message: "Welcome Back 🎉",
+                token
+            }
+        )
+
+    } catch (error) {
+        console.error("Login Error Context: ", error);
+        return res.status(500).json(
+            {
+                message: "Server Error Problem"
+            }
+        )
+    }
+}
+
+
+//  Exporting
 export {
-    signUp
+    signUp,
+    login
 }
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signUp } from "../controllers/authUser.js";
+import { login, signUp } from "../controllers/authUser.js";
 
 const router = Router()
 
 router.post("/signup",signUp)
+router.post("/login",login)
 
 export default router
