407d66dd7b7703998882318490bc5c4f62a7b609
Rashid <rkbooy623@gmail.com>
2026-08-31 20:24:43 +0530

    Add product model schema for managing product data

---
+++ src/models/product.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true,"Product title is required"],
+            trim: true
+        },
+        price: {
+            type: Number,
+            required: [true,"Product price is required"],
+            min: [0,"Price can't be negative"]
+        },
+        brand: {
+            type: String,
+            trim: true
+        },
+        category: {
+            type: String,
+            required: [true,"Product category is required"]
+        },
+        description: {
+            type: String,
+            required: [true,"Product description is required"],
+        },
+        specs: {
+           type: Map,
+           of: String
+        }
+    },
+    {
+        timestamps: true,
+        collection:"Products"
+    }
+)
+
+export const Product = mongoose.model("Product", productSchema)
