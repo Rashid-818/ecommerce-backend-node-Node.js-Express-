3f9419f8fea56406d7e1eb927d7b19e4c1c6c50b
Rashid <rkbooy623@gmail.com>
2026-09-02 11:59:40 +0530

    Refactor addToCart function: improve validation checks and error messages for better clarity

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,56 +1,67 @@
+import mongoose from "mongoose"
 import { Cart } from "../models/cart.model.js"
 import { Product } from "../models/product.model.js"
 
 const addToCart = async (req, res) => {
     try {
-
-        const { productId, quantity } = req.body
         const userId = req.user.userId
+        const { productId, quantity } = req.body
 
-        if (!productId || quantity === undefined || quantity < 1) {
+        if (!mongoose.isValidObjectId(userId)) {
+            return res.status(400).json({ message: "Invalid Id format" })
+        }
+
+        if (!productId || quantity === undefined) {
             return res.status(400).json(
                 {
-                    message: "valid product ID and quantity required"
+                    message: "Required fields are missing..."
                 }
             )
+        }
+        if (quantity < 1) {
+            return res.status(400).json(
+                {
+                    message: "Quantity must be at least 1"
+                }
+            )
+        }
+
+
+        if (!mongoose.isValidObjectId(productId)) {
+            return res.status(400).json({ message: "Invalid productId format" })
         }
 
         const product = await Product.findById(productId)
         if (!product) {
-            return res.status(404).json(
-                {
-                    message: "Product not found"
-                }
-            )
+            return res.status(404).json({ message: "Product not found" })
         }
 
         let cart = await Cart.findOne({ user: userId })
         if (!cart) {
-            cart = new Cart(
-                {
-                    user: userId,
-                    items: []
-                }
-            )
+            cart = new Cart({
+                user: userId,
+                items: []
+            })
         }
-
 
         const existingItems = cart.items.find(
             item => item.product.toString() === productId
         )
-
         if (existingItems) {
             existingItems.quantity += quantity
         } else {
-            cart.items.push({
-                product: productId,
-                quantity
-            })
+            cart.items.push(
+                {
+                    product: productId,
+                    quantity
+                }
+            )
         }
 
         await cart.save()
 
-        return res.status(201).json(
+
+        return res.status(200).json(
             {
                 message: "Product added to cart",
                 cart
@@ -59,6 +70,7 @@ const addToCart = async (req, res) => {
 
     } catch (error) {
         console.log(error);
+
         return res.status(500).json(
             {
                 message: "Server Error"
